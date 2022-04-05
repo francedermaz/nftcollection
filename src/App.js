b6ffaa16be492d3f9a7b0bb5fd7213cb25d8b2d2
francedermaz <francedermaz@gmail.com>
2022-04-05 17:33:06 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route exact path="/" element={<LandingPage />} /> */}
+        <Route exact path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );

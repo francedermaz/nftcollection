bf5732473c26a145b38569c6cf5c4873d4ead16e
francedermaz <francedermaz@gmail.com>
2022-04-05 17:31:56 -0300

    Basic navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
+import logo from './assets/logo.png';
+
+const NavBar = () => {
+    return(
+        <div>
+            <section className={styles.navbar}>
+                <div className={styles.link}>
+                    <Link to={'/'}>
+                        <img className={styles.img} src={logo} alt="logo"/>
+                    </Link>
+                </div>
+                <div className={styles.menu}>
+                    <Link to={'/home'}>
+                        <h1 className={styles.title}>Home</h1>
+                    </Link>
+                    <Link to={'/contact'}>
+                        <h1 className={styles.title}>Contact</h1>
+                    </Link>
+                    <div className={styles.div}>
+                        <Link to={'/account'}>
+                            <button>Connect</button>
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default NavBar;

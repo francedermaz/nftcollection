4995b878e00b4ec06984e8398241a84ae8e59994
francedermaz <francedermaz@gmail.com>
2022-04-05 18:01:47 -0300

    add styles

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -10,12 +10,12 @@ const Landing = () => {
             <div className={styles.page}>
                 <div className={styles.textland}>
                     <h1 className={styles.titleone}>Extraordinary</h1>
-                    <div>
-                        <h1 className={styles.nft}>NFT</h1>
+                    <div className={styles.divnftlove}>
+                        <h1 className={styles.nft}>NFT</h1><pre>  </pre>
                         <h1 className={styles.titleone}>you'll love</h1>
-                        <p className={styles.text}>I believe you have a unique taste, discover the</p>
-                        <p className={styles.text}>endless possibilities of the NFT world</p>
                     </div>
+                    <p className={styles.text}>I believe you have a unique taste, discover the</p>
+                    <p className={styles.text}>endless possibilities of the NFT world</p>
                     <Link to={'/home'}>
                         <button className={styles.button}>Explore now</button>
                     </Link>
